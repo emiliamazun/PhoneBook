f9adf250e2229b4f15689120fbce5bf2cc504e68
Aleksandra Skoczypiec <extern.Aleksandra.Skoczypiec@elektrobit.com>
2019-04-18 11:32:43 +0200

    Added solution for printing basic and extended info about Employee
    Added tests for checking basic and extended info

---
+++ PhoneBook/PhoneBook/Employee.cs
@@ -0,0 +1,34 @@
+﻿namespace PhoneBook
+{
+    public class Employee
+    {
+        public Employee(string name, string surname, string badgeID, string department, string internalPhone)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.BadgeID = badgeID;
+            this.Department = department;
+            this.InternalPhone = internalPhone;
+        }
+
+        public string Name { get; }
+
+        public string Surname { get; }
+
+        public string BadgeID { get; }
+
+        public string Department { get; }
+
+        public string InternalPhone { get; }
+
+        public string PrintBasicInfo()
+        {
+            return $"{Name} {Surname}";
+        }
+
+        public string PrintFullInfo()
+        {
+            return $"{Name} {Surname} {BadgeID} {Department} {InternalPhone}";
+        }
+    }
+}
